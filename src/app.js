bcc19630106ae4e1db4da5e72ff6f19a1fa33f4c
Aayush-M-1 <aayush1mahobia1@gmail.com>
2024-06-05 00:10:41 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public")) //for storing assets like pdfs, etc.
 app.use(cookieParser())
 
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration (using "use" instead of "get", because we use middlewares whenever files are separated)
+app.use("/api/v1/users", userRouter)
+
+// https://localhost:8000/api/v1/users/register
+
 export { app }
