127831e214b70c57747d1ef2537f414f24f2e57e
Aayush-M-1 <aayush1mahobia1@gmail.com>
2024-06-11 22:22:19 +0530

    some bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -265,7 +265,9 @@ const changeCurrentPassword = asyncHandler( async (req,res) => {
 const getCurrentUser = asyncHandler( async (req,res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(
+        new ApiResponse(200, req.user, "user fetched successfully")
+    )
 })
 
 const updateAccountDetails = asyncHandler( async (req,res) => {
@@ -275,7 +277,7 @@ const updateAccountDetails = asyncHandler( async (req,res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
