01eed98c01e64f11cca9082327d69ae3c952544e
Aayush-M-1 <aayush1mahobia1@gmail.com>
2024-06-13 22:29:54 +0530

    added other models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -35,9 +35,10 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+// for testing, /c/username
 
 router.route("/history").get(verifyJWT, getWatchHistory)
 
